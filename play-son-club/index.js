12179d83295aa46729c86b79cf8c3f2a6f04fa37
NGO VAN LOI Assistant/Smart Factory Development Team <loinv@lgcns.com>
2025-04-08 17:45:32 +0700

    play-son-club

--- play-son-club/index.js
+++ play-son-club/index.js
@@ -1,9 +1,13 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+import cron from "node-cron";
 import { PlaySonClubSite } from "./play.son.club.js";
-let site = new PlaySonClubSite();
-let token = await site.singIn("tienbip12345", "qweqwe123");
-console.log(`Login successfully.\nToken: ${token}`);
 
-let amount = 500000;
-let bankInfo = await site.getBankInfo(token, amount);
-console.log(bankInfo);
+cron.schedule("* * * * *", async () => {
+  let site = new PlaySonClubSite();
+  let token = await site.singIn("tienbip12345", "qweqwe123");
+  console.log(`Login successfully.\nToken: ${token}`);
+
+  let amount = 500000;
+  let bankInfo = await site.getBankInfo(token, amount);
+  console.log(bankInfo);
+});
